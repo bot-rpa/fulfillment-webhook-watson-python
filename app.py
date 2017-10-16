d9404f3fbc9c443811bf6d4c4d721362397b1d3b
bot-rpa <32352458+bot-rpa@users.noreply.github.com>
2017-10-16 10:58:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def makeWebhookResult(duration,servicetype):
     else:
         usage = "200"
     
-    output_speech = "Your " + servicetype + " usage for the duration " + duration + " is " + usage\n + " units which costs " + str(float(usage) * 0.45) + " pounds. Any thing else I can do for you."
+    output_speech = "Your " + servicetype + " usage for the duration " + duration + " is " + usage + " units which costs " + str(float(usage) * 0.45) + " pounds. Any thing else I can do for you."
     return {
         "speech": output_speech,
         "displayText": output_speech,
