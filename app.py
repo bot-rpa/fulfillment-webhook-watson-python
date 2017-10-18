16a65e6b41c6075716b95e293479affbefa4cf0d
bot-rpa <32352458+bot-rpa@users.noreply.github.com>
2017-10-18 12:16:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,10 +54,20 @@ def processRequest(req):
 def makeWebhookResult2(userid, password):
     if (userid == "arvind@gmail.com" and password == "Password123"):     
         username = "Arvind"
-        output_speech = "You have entered correct details . Welcome " + username
+        output_speech = "You have entered correct details . Welcome " + username  
+         return {
+        "speech": output_speech,
+        "displayText": output_speech,
+        "source": "apiai-weather-webhook-sample"
+  }
     elif (userid == "sree@gmail.com" and password == "Password123"):     
         username = "Sree" 
         output_speech = "You have entered correct details . Welcome " + username
+         return {
+        "speech": output_speech,
+        "displayText": output_speech,
+        "source": "apiai-weather-webhook-sample"
+  }
     else:
         output_speech = "You haven't entered correct details. Please re-enter the credentials"
     return {
