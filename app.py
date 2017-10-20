91bf5c483fd5a451ed32c1ef9368d76296b88baf
bot-rpa <32352458+bot-rpa@users.noreply.github.com>
2017-10-20 17:37:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,8 +94,10 @@ def makeWebhookResult2(userid, password):
                                 }
                             ]
                         }
-                    ]
+                    }
                 }
+            ]
+        }                }
     elif (userid == "88888" and password == "password123"):     
         username = "Sree" 
         output_speech = "You have entered correct details . Welcome " + username
