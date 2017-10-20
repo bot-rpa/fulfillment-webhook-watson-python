87c61c6dccdd37a697579eafe746677cbcf48e7f
bot-rpa <32352458+bot-rpa@users.noreply.github.com>
2017-10-20 15:11:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-    
-    from __future__ import print_function
-    from future.standard_library import install_aliases
-    
-    install_aliases()
-    
-    from urllib.parse import urlparse, urlencode
-    from urllib.request import urlopen, Request
-    from urllib.error import HTTPError
-    
-    import json
-    import os
-    
-    from flask import Flask
-    from flask import request
-    from flask import make_response
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+import smtplib
+
+from flask import Flask
+from flask import request
+from flask import make_response
     
     # Flask app should start in global layout
     app = Flask(__name__)
